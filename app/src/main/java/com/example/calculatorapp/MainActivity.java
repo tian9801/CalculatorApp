fe374f84dccdd4a596aece6ef97f2812ff02d41f
tian9801 <tian9801@students.d211.org>
2022-08-23 12:51:10 -0500

    fixed factorial and bugs, meets base criteria + factorial function

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -50,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
         numberSumTV.setText("" + product);
     }
-
     public void findQuotient(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
@@ -58,9 +57,24 @@ public class MainActivity extends AppCompatActivity {
 
         int num1 = Integer.parseInt((number1ET.getText().toString()));
         int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int quotient = num1 + num2;
+        double quotient = num1 / num2;
 
         numberSumTV.setText("" + quotient);
     }
 
+    public void findFactorial(View view) {
+        //calculates the factorial of the number given
+        EditText number1ET = findViewById(R.id.num1ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int factorial = Integer.parseInt((number1ET.getText().toString()));
+        int num = 1; //starting from 1
+        for(int i = factorial; i > 0; i--){
+            //the num entered, factorial, is the beginning num. for each iteration of the loop, it is multiplied
+            //by the number with a value of 1 less
+            num *= i;
+        }
+        factorial = num;
+        numberSumTV.setText("" + factorial);
+    }
 }
