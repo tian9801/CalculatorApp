cd7704bdeafc965fdd330a83a675d332c6bc8d90
tian9801 <tian9801@students.d211.org>
2022-08-24 12:55:13 -0500

    sort of works need to change button icons! new vector asset

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -21,15 +21,18 @@ public class MainActivity extends AppCompatActivity {
         TextView numberSumTV = findViewById(R.id.resultTV);
         int num1;
         int num2;
+        //try catch block to prevent crashes if the item the user entered is not an integer
         try {
             num1 = Integer.parseInt((number1ET.getText().toString()));
+            numberSumTV.setText("" + num1);
             num2 = Integer.parseInt((number2ET.getText().toString()));
             int sum = num1 + num2;
+            numberSumTV.setText("" + numberSumTV.getText() + " + " + num2);
 
-            numberSumTV.setText("" + sum);
+            numberSumTV.setText("" + numberSumTV.getText() + " = " + sum);
         }
         catch(NumberFormatException e){
-            numberSumTV.setText("You didn't enter a number");
+            numberSumTV.setText("You didn't enter a number! Try again.");
             }
 
     }
@@ -38,65 +41,104 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberDiffTV = findViewById(R.id.resultTV);
+        int num1;
+        int num2;
+        //try catch block to prevent crashes if the item the user entered is not an integer
+        try {
+            num1 = Integer.parseInt((number1ET.getText().toString()));
+            num2 = Integer.parseInt((number2ET.getText().toString()));
+            int diff = num1 - num2;
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int difference = num1 - num2;
+            numberDiffTV.setText("" + diff);
+        }
+        catch(NumberFormatException e){
+            numberDiffTV.setText("You didn't enter a number! Try again.");
+        }
 
-        numberDiffTV.setText("" + difference);
     }
 
     public void findProduct(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberProdTV = findViewById(R.id.resultTV);
+        int num1;
+        int num2;
+        //try catch block to prevent crashes if the item the user entered is not an integer
+        try {
+            num1 = Integer.parseInt((number1ET.getText().toString()));
+            num2 = Integer.parseInt((number2ET.getText().toString()));
+            int prod = num1 * num2;
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int product = num1 * num2;
-
-        numberProdTV.setText("" + product);
+            numberProdTV.setText("" + prod);
+        }
+        catch(NumberFormatException e){
+            numberProdTV.setText("You didn't enter a number! Try again.");
+        }
     }
     public void findQuotient(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberQuotientTV = findViewById(R.id.resultTV);
+        int num1;
+        int num2;
+        //try catch block to prevent crashes if the item the user entered is not an integer
+        try {
+            num1 = Integer.parseInt((number1ET.getText().toString()));
+            num2 = Integer.parseInt((number2ET.getText().toString()));
+            int quotient = num1 / num2;
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        double quotient = num1 / num2;
-
-        numberQuotientTV.setText("" + quotient);
+            numberQuotientTV.setText("" + quotient);
+        }
+        catch(NumberFormatException e){
+            numberQuotientTV.setText("You didn't enter a number! Try again.");
+        }
     }
 
     public void findFactorial(View view) {
         //calculates the factorial of the number given
         EditText number1ET = findViewById(R.id.num1ET);
         TextView numberFactorialTV = findViewById(R.id.resultTV);
-
-        int factorial = Integer.parseInt((number1ET.getText().toString()));
-        int num = 1; //starting from 1
-        for(int i = factorial; i > 0; i--){
-            //the num entered, factorial, is the beginning num. for each iteration of the loop, it is multiplied
-            //by the number with a value of 1 less
-            num *= i;
+        int num;
+        int factorial;
+        //try catch block to prevent crashes if the item the user entered is not an integer
+        try {
+            factorial = Integer.parseInt((number1ET.getText().toString()));
+            num = 1; //starting from 1
+            for(int i = factorial; i > 0; i--){
+                //the num entered, factorial, is the beginning num. for each iteration of the loop, it is multiplied
+                //by the number with a value of 1 less
+                num *= i;
+            }
+            factorial = num;
+            numberFactorialTV.setText("" + factorial);
         }
-        factorial = num;
-        numberFactorialTV.setText("" + factorial);
+        catch(NumberFormatException e){
+            numberFactorialTV.setText("You didn't enter a number! Try again.");
+        }
+
+
     }
 
     public void findExponent(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberExpTV = findViewById(R.id.resultTV);
+        int num1;
+        int num2;
+        //try catch block to prevent crashes if the item the user entered is not an integer
+        try {
+            num1 = Integer.parseInt((number1ET.getText().toString()));
+            num2 = Integer.parseInt((number2ET.getText().toString()));
+            int exp = 1;
+            for(int i = 0; i < num2; i++){
+                exp = exp * num1;
+            }
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int exp = 1;
-        for(int i = 0; i < num2; i++){
-            exp = exp * num1;
+            numberExpTV.setText("" + exp);
+        }
+        catch(NumberFormatException e){
+            numberExpTV.setText("You didn't enter a number! Try again.");
         }
 
-        numberExpTV.setText("" + exp);
     }
 }
