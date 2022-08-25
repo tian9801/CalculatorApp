293c9fa74232dff0961cd1479286d244e06c0731
tian9801 <tian9801@students.d211.org>
2022-08-25 12:33:21 -0500

    finished project, new buttons , comments, equation display

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findSum(View view) {
+        //calculates sum of num1 and num2
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberSumTV = findViewById(R.id.resultTV);
@@ -23,12 +24,13 @@ public class MainActivity extends AppCompatActivity {
         int num2;
         //try catch block to prevent crashes if the item the user entered is not an integer
         try {
+            //check if entered values are ints
             num1 = Integer.parseInt((number1ET.getText().toString()));
             numberSumTV.setText("" + num1);
             num2 = Integer.parseInt((number2ET.getText().toString()));
             int sum = num1 + num2;
             numberSumTV.setText("" + numberSumTV.getText() + " + " + num2);
-
+            //adds onto message to display the calculated equation at the end
             numberSumTV.setText("" + numberSumTV.getText() + " = " + sum);
         }
         catch(NumberFormatException e){
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findDiff(View view) {
+        //calculates difference of num1 and num2
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberDiffTV = findViewById(R.id.resultTV);
@@ -45,11 +48,15 @@ public class MainActivity extends AppCompatActivity {
         int num2;
         //try catch block to prevent crashes if the item the user entered is not an integer
         try {
+            //check if entered values are ints
             num1 = Integer.parseInt((number1ET.getText().toString()));
+            numberDiffTV.setText("" + num1);
             num2 = Integer.parseInt((number2ET.getText().toString()));
+            numberDiffTV.setText("" + numberDiffTV.getText() + " - " + num2);
             int diff = num1 - num2;
 
-            numberDiffTV.setText("" + diff);
+            numberDiffTV.setText("" + numberDiffTV.getText() + " = " + diff);
+            //displays equation calculated at the end
         }
         catch(NumberFormatException e){
             numberDiffTV.setText("You didn't enter a number! Try again.");
@@ -58,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findProduct(View view) {
+        //calculates product of num1 and num2
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberProdTV = findViewById(R.id.resultTV);
@@ -65,17 +73,22 @@ public class MainActivity extends AppCompatActivity {
         int num2;
         //try catch block to prevent crashes if the item the user entered is not an integer
         try {
+            //check if entered values are ints
             num1 = Integer.parseInt((number1ET.getText().toString()));
+            numberProdTV.setText("" + num1);
             num2 = Integer.parseInt((number2ET.getText().toString()));
+            numberProdTV.setText("" + numberProdTV.getText() + " x " + num2);
             int prod = num1 * num2;
 
-            numberProdTV.setText("" + prod);
+            numberProdTV.setText("" + numberProdTV.getText() + " = " + prod);
+            //displays final equation calculated
         }
         catch(NumberFormatException e){
             numberProdTV.setText("You didn't enter a number! Try again.");
         }
     }
     public void findQuotient(View view) {
+        //calculates quotient of num1 and num2
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberQuotientTV = findViewById(R.id.resultTV);
@@ -83,11 +96,12 @@ public class MainActivity extends AppCompatActivity {
         int num2;
         //try catch block to prevent crashes if the item the user entered is not an integer
         try {
+            //check if entered values are ints
             num1 = Integer.parseInt((number1ET.getText().toString()));
             num2 = Integer.parseInt((number2ET.getText().toString()));
             int quotient = num1 / num2;
 
-            numberQuotientTV.setText("" + quotient);
+            numberQuotientTV.setText("" + num1 + " ÷ " + num2 + " = " + quotient);
         }
         catch(NumberFormatException e){
             numberQuotientTV.setText("You didn't enter a number! Try again.");
@@ -95,13 +109,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findFactorial(View view) {
-        //calculates the factorial of the number given
+        //calculates the factorial of the number given in slot 1
         EditText number1ET = findViewById(R.id.num1ET);
         TextView numberFactorialTV = findViewById(R.id.resultTV);
         int num;
         int factorial;
         //try catch block to prevent crashes if the item the user entered is not an integer
         try {
+            //check if entered values are ints
             factorial = Integer.parseInt((number1ET.getText().toString()));
             num = 1; //starting from 1
             for(int i = factorial; i > 0; i--){
@@ -110,7 +125,8 @@ public class MainActivity extends AppCompatActivity {
                 num *= i;
             }
             factorial = num;
-            numberFactorialTV.setText("" + factorial);
+            numberFactorialTV.setText("" + num + "! = " + factorial);
+            //displays full calculation
         }
         catch(NumberFormatException e){
             numberFactorialTV.setText("You didn't enter a number! Try again.");
@@ -120,6 +136,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findExponent(View view) {
+        //calculates num1 raised to the power of num2
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
         TextView numberExpTV = findViewById(R.id.resultTV);
@@ -127,6 +144,7 @@ public class MainActivity extends AppCompatActivity {
         int num2;
         //try catch block to prevent crashes if the item the user entered is not an integer
         try {
+            //check if entered values are ints
             num1 = Integer.parseInt((number1ET.getText().toString()));
             num2 = Integer.parseInt((number2ET.getText().toString()));
             int exp = 1;
@@ -134,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
                 exp = exp * num1;
             }
 
-            numberExpTV.setText("" + exp);
+            numberExpTV.setText("" + num1 + "^" + num2 + " = " + exp);
         }
         catch(NumberFormatException e){
             numberExpTV.setText("You didn't enter a number! Try again.");
